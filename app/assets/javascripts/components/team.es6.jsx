bc81a4ddc26053032b866fb42ad984c2c38ddc54
Guilherme Pontes <guilhermepontes@msn.com>
2015-09-27 23:30:48 -0300

    Team info component.

---
+++ app/assets/javascripts/components/team.es6.jsx
@@ -0,0 +1,48 @@
+class Team extends React.Component {
+ constructor(){
+    super();
+
+    this.teamMap = {
+      blue: 100,
+      red: 200
+    };
+
+    this.endpoint  = 'http://ddragon.leagueoflegends.com/cdn/5.18.1/data/en_US/champion.json';
+    this.imgPrefix = 'http://ddragon.leagueoflegends.com/cdn/5.18.1/img/champion/';
+    this.state     = { champions: '' };
+  }
+
+
+  componentDidMount(){
+    $.get(this.endpoint, function(data) {
+      this.setState({
+        champions: data
+      });
+    }.bind(this));
+  }
+
+  render(){
+    let props   = this.props;
+    let teamMap = this.teamMap;
+    let classes = React.addons.classSet('game', props.color);
+    let prefix  = this.imgPrefix;
+    let champs  = this.champions;
+
+    return (
+      <div className={ classes }>
+        <ul>
+          {
+            props.participants.filter(function(player){
+              return player.teamId === teamMap[props.color];
+            }).map(function(player){
+              return <li>
+                { player.summonerName }
+              </li>
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+
+}
