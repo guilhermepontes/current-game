ad581ac2f8d8648baee154a7576747550c0e3f58
Guilherme Pontes <guilhermepontes@msn.com>
2015-09-27 23:30:35 -0300

    Header component.

---
+++ app/assets/javascripts/components/header.es6.jsx
@@ -0,0 +1,11 @@
+class Header extends React.Component {
+  render(){
+    return (
+      <header className="header">
+        <h1 className="logo">
+          <a href="/">current game</a>
+        </h1>
+      </header>
+    );
+  }
+}
