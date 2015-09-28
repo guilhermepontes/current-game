7f921ad33d5eba55f29e79bdbaa2d24a8eb8a2c4
Guilherme Pontes <guilhermepontes@msn.com>
2015-09-27 23:27:16 -0300

    Current game controller.

---
+++ app/controllers/current_controller.rb
@@ -0,0 +1,18 @@
+class CurrentController < ApplicationController
+  def index
+    render component: 'Form', props: {
+      action: search_path,
+      csrf_param: request_forgery_protection_token,
+      csrf_token: form_authenticity_token
+    }
+  end
+
+  def search
+    id = Summoner.id_by_nick(params[:nick])
+
+    client = Taric.client(region: :br)
+    info = client.current_game(summoner_id: id)
+
+    render component: 'Game', props: info
+  end
+end
