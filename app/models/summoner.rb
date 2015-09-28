6305915f1ba339831966b65ddabc75d5ae7d8d0c
Guilherme Pontes <guilhermepontes@msn.com>
2015-09-27 23:24:30 -0300

    Summoner model.

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,6 +1,14 @@
 class Summoner < ActiveRecord::Base
-  attribute :id, Integer
-  attribute :name, String
-  attribute :profile_icon_id, Integer
-  attribute :summoner_level, Integer
+  attr_accessor :id, :name, :profile_icon_id, :summoner_level, :revision_date
+
+  def self.find(nick)
+    client = Taric.client(region: :br)
+
+    params = client.summoners_by_names(summoner_names: nick)[nick.downcase.split.join].transform_keys(&:underscore)
+    self.new(params)
+  end
+
+  def self.id_by_nick(nick)
+    find(nick).id
+  end
 end
